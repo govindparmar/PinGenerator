b09bcad25578711454d7becdb9826f0afd149840
Govind Parmar <Govind@GOVIND-PC>
2020-07-28 14:24:47 -0600

    Renamed "Form1" to "MainForm"

---
+++ PinGenerator/MainForm.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPin = New System.Windows.Forms.TextBox()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CSPRNG-generated pin:"
+        '
+        'txtPin
+        '
+        Me.txtPin.Location = New System.Drawing.Point(3, 26)
+        Me.txtPin.Name = "txtPin"
+        Me.txtPin.Size = New System.Drawing.Size(354, 20)
+        Me.txtPin.TabIndex = 1
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(261, 52)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(96, 26)
+        Me.btnGenerate.TabIndex = 2
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(359, 78)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.txtPin)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.Text = "Pin Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPin As TextBox
+    Friend WithEvents btnGenerate As Button
+End Class
---
+++ PinGenerator/MainForm.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class MainForm
+    Private ReadOnly digits() As Byte = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    Dim seen = New Byte() {0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+    Private Function IsFinished() As Boolean
+
+        For i As Byte = 0 To 8
+            If seen(i) = 0 Then
+                Return False
+            End If
+        Next
+
+        Return True
+    End Function
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim sb As StringBuilder
+        Dim rngByte(1) As Byte
+
+        seen = New Byte() {0, 0, 0, 0, 0, 0, 0, 0, 0}
+        Using csprng = New RNGCryptoServiceProvider
+            sb = New StringBuilder
+
+            While Not IsFinished()
+                csprng.GetBytes(rngByte)
+                rngByte(0) = 1 + rngByte(0) Mod 9
+
+                If seen(rngByte(0) - 1) = 0 Then
+                    seen(rngByte(0) - 1) = 1
+                    sb.Append(rngByte(0).ToString)
+                End If
+
+            End While
+
+            txtPin.Text = sb.ToString
+        End Using
+    End Sub
+End Class
